a0b5a2e98345b1ae1a55c7dd0d53f284ac1203e5
varduhi94 <grigoryanvarduhi94@gmail.com>
2018-12-08 14:48:37 +0400

    Add param. in method

--- vidly/vidly/App_Start/RouteConfig.cs
+++ vidly/vidly/App_Start/RouteConfig.cs
@@ -12,7 +12,11 @@ namespace vidly
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new {controller = "Movies", action = "ByReleaseDate" },
+                new { year = @"\d{4}", month = @"\d{2}" });
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- vidly/vidly/Controllers/MoviesController.cs
+++ vidly/vidly/Controllers/MoviesController.cs
@@ -16,5 +16,27 @@ namespace vidly.Controllers
 
             return View(movie);
         }
+        public ActionResult Edit (int movieId)
+        {
+            return Content("id=" + movieId);
+        }
+        // movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(String.Format("pageIndex = {0}&sortBy={1}", pageIndex, sortBy));
+        }
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }  
+
     }
 }
